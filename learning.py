f92c0ab5e968f5180c1170224ac9fe57ef5c2563
Emmanuel M. Paul <pauldukz@gmail.com>
2021-05-24 15:39:41 +0300

    add color

--- learning.py
+++ learning.py
@@ -1,33 +1,58 @@
 #!/usr/bin/env python3
 import pyttsx3
-import sys,time,os
+import colorama
+import sys
+import time
+import os
 from time import sleep
 from threading import Thread
 
+colorama.init()
+print(colorama.Style.BRIGHT, end="")
+
 engine = pyttsx3.init()
-voices = engine.getProperty('voices') 
-engine.setProperty('volume',1.0)
+voices = engine.getProperty('voices')
+engine.setProperty('volume', 1.0)
 engine.setProperty('voice', voices[int(sys.argv[2])].id)
 
-def play_sound(mytext): 
-  engine.say(mytext)
-  engine.runAndWait()
+
+def play_sound(mytext):
+    engine.say(mytext)
+    engine.runAndWait()
+
 
 def write_to_screen(mytext):
-  sprint(mytext)
+    if len(mytext.strip()) != 0 and mytext[0] == '(':
+        print(colorama.Fore.RED, mytext, end=" ")
+    elif len(mytext.strip()) != 0 and mytext.lstrip()[0] == '-' :
+        print(colorama.Fore.GREEN, mytext, end=" ")
+    elif len(mytext.strip()) != 0 and mytext.lstrip()[0] == '*' :
+        print(colorama.Fore.MAGENTA, mytext, end=" ")
+    elif len(mytext.strip()) != 0 and mytext.lstrip()[0] == '~' :
+        print(colorama.Fore.BLUE, mytext, end=" ")
+    elif len(mytext.strip()) != 0 and mytext.lstrip()[0] == '+' :
+        print(colorama.Fore.CYAN, mytext, end=" ")
+    else:
+        print(mytext, end="")
+    
+    print(colorama.Fore.WHITE, "", end="")
+
 
 now = time.time()
+
+
 def sprint(str):
-   for c in str:
-     sys.stdout.write(c)
-     sys.stdout.flush()
-     sleep(1./115)
+    for c in str:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        sleep(1./115)
+
 
 file1 = open(str(sys.argv[1]), 'r')
 Lines = file1.readlines()
-for line in Lines: 
-    Thread(target = write_to_screen(line)).start() 
-    Thread(target = play_sound(line.strip())).start() 
+for line in Lines:
+    Thread(target=write_to_screen(line)).start()
+    Thread(target=play_sound(line.strip())).start()
 file1.close()
 
 later = time.time()
